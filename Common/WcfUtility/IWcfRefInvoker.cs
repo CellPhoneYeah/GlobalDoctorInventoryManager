3fbc206918f9bf86fc4376b313b4283491851a43
叶晓峰 <13751732491@163.com>
2017-08-23 17:36:31 +0800

    动态调用服务端类完善中。。。

---
+++ Common/WcfUtility/IWcfRefInvoker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+
+namespace Common
+{
+    public interface IWcfRefInvoker
+    {
+        Binding GetBinding();
+        
+        T CreateWCFServiceByUrl<T>(string url,string binding)
+    }
+}
